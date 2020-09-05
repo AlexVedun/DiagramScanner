17629801748d74f4cf3944bc0cdc97db22f49c9c
Efremenko Alex <alexeyefr@gmail.com>
2020-09-05 17:40:52 +0300

    Add: make the first look

--- DiagramScanner/MainWindow.xaml.cs
+++ DiagramScanner/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace DiagramScanner
         {
             InitializeComponent();
         }
+
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
