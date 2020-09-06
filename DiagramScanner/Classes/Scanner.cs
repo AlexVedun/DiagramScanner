c1885b95fe73b431cf2ddea9f379725624f53df3
Efremenko Alex <alexeyefr@gmail.com>
2020-09-06 13:56:34 +0300

    Add: Scanner class

---
+++ DiagramScanner/Classes/Scanner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace DiagramScanner.Classes
+{
+    class Scanner
+    {
+        public Scanner DiagramScanner { get; set; }
+        public Image DiagramImage { get; set; }
+        public Canvas MainCanvas { get; set; }
+        public void OpenImage(string fileName)
+        {
+            ImageSource diagramImage = new BitmapImage(new Uri(fileName));
+            MainCanvas.Width = diagramImage.Width;
+            MainCanvas.Height = diagramImage.Height;
+            DiagramImage.Source = diagramImage;
+        }
+    }
+}
--- DiagramScanner/MainWindow.xaml.cs
+++ DiagramScanner/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DiagramScanner.Classes;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,14 +22,44 @@ namespace DiagramScanner
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Scanner scanner = new Scanner();
         public MainWindow()
         {
             InitializeComponent();
+
+            scanner.MainCanvas = MainCanvas;
+            scanner.DiagramImage = MainImage;
         }
 
         private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void OpenFileMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg;*.bmp)|*.png;*.jpeg;*bmp|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                scanner.OpenImage(openFileDialog.FileName);
+            }
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Polyline polyline = new Polyline();
+            PointCollection points = new PointCollection();
+            points.Add(new Point(0, 0));
+            points.Add(new Point(5, 10));
+            points.Add(new Point(20, 8));
+            points.Add(new Point(35, 20));
+            points.Add(new Point(40, 40));
+            points.Add(new Point(100, 100));
+            polyline.Points = points;
+            polyline.Stroke = new SolidColorBrush(Colors.Blue);
+            MainCanvas.Children.Add(polyline);
         }
     }
 }
